9b4d5dc2cb568d0cc042c12ee316826e973bfd19
lucas.delazeri <lucas.delazeri@universo.univates.br>
2024-09-26 21:31:41 -0300

    alteracoes no celulares.java

--- celulares.java
+++ celulares.java
@@ -1,6 +1,29 @@
-int celNumber = 921321372109312;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-if( celNumber =! 0 )
-{
-	System.out.println("bomdia");
-}
+public class celulares{
+
+	public static void main(String[] args) {
+		
+		int celNumber = 411;
+		
+		List<Integer> a = new ArrayList<Integer>();
+		
+		Scanner s1 = new Scanner(System.in);
+		
+		for( int i = 0; i < 10000; i++ )
+		{
+			a.add( s1.nextInt() );	
+		}
+		if( celNumber != 0 )
+		{
+			
+			System.out.println("bomdia");
+		}
+	
+	}
+	
+
+}	
+
